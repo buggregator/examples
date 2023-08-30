e885d6c44156e59219bc92f2919cec3dee90a86e
butschster <butschster@gmail.com>
2023-08-30 12:01:22 +0400

    Fixes link to Buggregator github repository

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                 <img src="/images/logo.png" alt="{{ config('app.name') }} logo">
             </a>
 
-            <a class="py-2 px-3" href="https://github.com/buggregator/app" target="_blank">
+            <a class="py-2 px-3" href="https://github.com/buggregator/server" target="_blank">
                 <svg class="w-6 h-6 fill-current" viewBox="0 0 1024 1024" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd"
